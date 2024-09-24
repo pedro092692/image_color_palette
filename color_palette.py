e87cba6652d34ec495041935f2386c145a542f99
pedro092692 <pedro_092692@hotmail.com>
2024-09-24 11:43:47 -0400

    add socketio

--- color_palette.py
+++ color_palette.py
@@ -1,16 +1,18 @@
 from Pylette import extract_colors
-
+import os
 
 class GetColor:
 
-    def __init__(self):
-        self.palette = extract_colors(image='demo.png', palette_size=7)
+    def __init__(self, file):
+        self.palette = extract_colors(image=f'images/{file}', palette_size=7)
         self.most_common_color = []
 
         for color in self.palette:
             self.most_common_color.append(color.rgb)
 
-        print(self.most_common_color)
+        self.remove_files(file=file)
 
 
-
+    @staticmethod
+    def remove_files(file):
+        os.remove(f'images/{file}')
--- main.py
+++ main.py
@@ -1,16 +1,30 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_dropzone import Dropzone
+from color_palette import GetColor
+from flask_socketio import SocketIO
+import os
 
 app = Flask(__name__)
+socketio = SocketIO(app)
+
 
 # plugins
 dropzone = Dropzone(app)
+app.config['DROPZONE_ALLOWED_FILE_CUSTOM'] = True
+app.config['DROPZONE_ALLOWED_FILE_TYPE'] = '.jpg, .jpeg, .png, .bmp, .tiff, .webp, .svg'
+app.config['DROPZONE_MAX_FILE_SIZE'] = 8
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
+    if request.method == 'POST':
+        if request.files:
+            file = request.files.get('file')
+            file.save(os.path.join('images', file.filename))
+            img_colors = GetColor(file=file.filename)
+
     return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, debug=True)
